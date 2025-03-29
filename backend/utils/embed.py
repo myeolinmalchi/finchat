375c654928328014ba3efe548c88eac11471b9eb
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-03-30 08:06:38 +0900

    feat: 텍스트 임베딩 및 리랭커 서버 요청 함수 구현

--- backend/utils/embed.py
+++ backend/utils/embed.py
@@ -1,13 +1,14 @@
 from typing import List, overload
 
+from aiohttp import ClientSession
 from dotenv import load_dotenv
 import os
 
 from pydantic import TypeAdapter
 import requests
 
-from schemas.embed import EmbedResult
 from mixins.asyncio import retry_async, retry_sync
+from schemas.embed import EmbedResult, RerankResult
 
 load_dotenv()
 
@@ -43,6 +44,7 @@ def create_embedding(
     truncate: bool = True,
     html: bool = False,
 ) -> EmbedResult | List[EmbedResult]:
+
     body = {
         "inputs": texts,
         "chunking": chunking,
@@ -56,3 +58,56 @@ def create_embedding(
         return es.validate_json(raw)
 
     raise Exception("텍스트 임베딩에 실패했습니다.")
+
+
+@retry_async(delay=3)
+async def create_embedding_async(
+    texts: str | List[str],
+    session: ClientSession,
+    chunking: bool = True,
+    truncate: bool = True,
+    html: bool = True,
+) -> EmbedResult | List[EmbedResult]:
+
+    body = {
+        "inputs": texts,
+        "chunking": chunking,
+        "truncate": truncate,
+        "html": html,
+    }
+
+    async with session.post(f"{EMBED_URL}/embed", json=body) as res:
+        if res.status == 200:
+            raw = await res.read()
+            return es.validate_json(raw)
+
+        raise Exception("텍스트 임베딩에 실패했습니다.")
+
+
+rs = TypeAdapter(List[RerankResult])
+
+
+@retry_async(delay=3)
+async def rerank_async(
+    query: str,
+    texts: List[str],
+    session: ClientSession,
+    truncate: bool = True,
+    truncation_direction="Right",
+) -> List[RerankResult]:
+    if not texts:
+        return []
+
+    body = {
+        "query": query,
+        "texts": texts,
+        "truncate": truncate,
+        "truncation_direction": truncation_direction,
+    }
+
+    async with session.post(f"{EMBED_URL}/rerank", json=body) as res:
+        if res.status == 200:
+            raw = await res.read()
+            return rs.validate_json(raw)
+
+        raise Exception("Failed Reranking")
