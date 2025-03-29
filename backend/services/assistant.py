485a1f2ad25abd0cdc0fd5676b79882c56793ea4
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-03-30 08:04:56 +0900

    feat: 어시스턴트 서비스의 인터페이스 정의

--- backend/services/assistant.py
+++ backend/services/assistant.py
@@ -1,18 +1,25 @@
+from abc import abstractmethod
 import inspect
 import json
-from typing import List
+from typing import Generic, List, Optional, TypeVar
 
+from aiohttp import ClientSession
 from openai import AsyncClient
 from openai.types.chat import ChatCompletionMessageParam
 from agents import kb0
+from agents.schemas import KB0_Answer
 from services.search import AppSearchService, init_search_service
-from utils.embed import embed
+from utils.embed import create_embedding_async
 from itertools import chain
 
 import asyncio
 
+from mixins.http import HTTPMetaclass
 
-class AssistantService:
+AssistantAnswerT = TypeVar("AssistantAnswerT")
+
+
+class IAssistantService(Generic[AssistantAnswerT], metaclass=HTTPMetaclass):
 
     def __init__(
         self,
@@ -37,8 +44,41 @@ class AssistantService:
     async def pipeline_async(
         self,
         question: str,
-        history: List[ChatCompletionMessageParam] = [],
+        history: List[ChatCompletionMessageParam],
+        session: Optional[ClientSession] = None,
     ):
+        if not session:
+            raise ValueError
+
+        return await self._pipeline_async(question, history, session)
+
+    @abstractmethod
+    async def _pipeline_async(
+        self,
+        question: str,
+        history: List[ChatCompletionMessageParam],
+        session: ClientSession,
+    ) -> AssistantAnswerT:
+        pass
+
+
+class AssistantService(IAssistantService[str]):
+
+    def __init__(
+        self,
+        kb0: kb0.KB0,
+        kb0_1: kb0.KB0_1,
+        kb0_2: kb0.KB0_2,
+        kb0_4: kb0.KB0_4,
+        search_service: AppSearchService,
+    ):
+        self.kb0 = kb0
+        self.kb0_1 = kb0_1
+        self.kb0_2 = kb0_2
+        self.kb0_4 = kb0_4
+        self.search_service = search_service
+
+    async def _pipeline_async(self, question, history, session):
 
         print("before extract sub questions...")
         kb0_1_response = await self.kb0_1.inference(question, history)
@@ -49,8 +89,9 @@ class AssistantService:
         sub_questions = kb0_1_response.sub_questions
 
         print("embed sub questions...")
-        sub_question_embeddings = embed(
+        sub_question_embeddings = await create_embedding_async(
             sub_questions,
+            session=session,
             chunking=False,
             html=False,
         )
@@ -106,33 +147,9 @@ class AssistantService:
         return final_answer
 
 
-class AssistantServiceV2:
+class AssistantServiceV2(IAssistantService[List[KB0_Answer]]):
 
-    def __init__(
-        self,
-        kb0: kb0.KB0,
-        kb0_1: kb0.KB0_1,
-        kb0_2: kb0.KB0_2,
-        kb0_4: kb0.KB0_4,
-        search_service: AppSearchService,
-    ):
-        self.kb0 = kb0
-        self.kb0_1 = kb0_1
-        self.kb0_2 = kb0_2
-        self.kb0_4 = kb0_4
-        self.search_service = search_service
-
-    async def call_by_name(self, tool_name: str, **args) -> List[str]:
-        func = getattr(self.search_service, tool_name)
-        if inspect.iscoroutinefunction(func):
-            return await func(**args)
-        return func(**args)
-
-    async def pipeline_async(
-        self,
-        question: str,
-        history: List[ChatCompletionMessageParam] = [],
-    ):
+    async def _pipeline_async(self, question, history, session):
 
         print("before extract sub questions...")
         kb0_1_response = await self.kb0_1.inference(question, history)
@@ -143,8 +160,9 @@ class AssistantServiceV2:
         sub_questions = kb0_1_response.sub_questions
 
         print("embed sub questions...")
-        sub_question_embeddings = embed(
+        sub_question_embeddings = await create_embedding_async(
             sub_questions,
+            session=session,
             chunking=False,
             html=False,
         )
@@ -191,20 +209,11 @@ class AssistantServiceV2:
 
         if not kb0_response:
             raise ValueError
-        """
-
-        url2md = lambda url: f"[FILE]({url})" if "download" in url else f"[URL]({url})"
-        final_answer = "\n\n".join([
-            answer.paragraph + " " + "".join(map(url2md, answer.urls)) for answer in kb0_response.answers
-        ])
-
-        return final_answer
-        """
 
         return kb0_response.answers
 
 
-def init_assistant():
+def init_assistant() -> IAssistantService:
     search_service = init_search_service()
 
     client = AsyncClient()
