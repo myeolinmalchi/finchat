6524e707504feb510fac15b8d50b94f618791d26
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-07-16 12:59:19 +0900

    feat: 적금 검색 워크플로우를 서브그래프로 분리

---
+++ backend/domains/saving/agents/saving_subgraph.py
@@ -0,0 +1,41 @@
+from typing import List
+from langchain_core.language_models import BaseChatModel
+from langchain_core.tools import BaseTool
+from langgraph.graph import StateGraph
+
+from domains.common.agents.graph_state import GraphState
+
+from domains.saving.agents.router_node import init_router_node
+from domains.saving.agents.filter_node import init_filter_node
+from domains.saving.agents.tool_selection_node import init_saving_tool_seleciton_node
+
+
+def init_saving_subgraph(
+    llm: BaseChatModel,
+    saving_tools: List[BaseTool],
+):
+    """적금 서브그래프 초기화"""
+
+    sg = StateGraph(GraphState)
+
+    sg.add_node("tool_selection_node",
+                init_saving_tool_seleciton_node(llm, saving_tools))
+    #sg.add_node("tool_execution_node", init_saving_tool_execution_node(saving_tools))
+    #sg.add_edge("tool_selection_node", "tool_execution_node")
+
+    sg.add_node("filter_node", init_filter_node(llm))
+    sg.add_node("router_node", init_router_node())
+
+    sg.add_edge("tool_selection_node", "filter_node")
+
+    #sg.add_edge("tool_execution_node", "filter_node")
+    sg.add_edge("filter_node", "router_node")
+
+    sg.add_conditional_edges("router_node", lambda state: state["next"])
+
+    sg.set_entry_point("tool_selection_node")
+    sg.set_finish_point("router_node")
+
+    graph = sg.compile()
+
+    return graph
