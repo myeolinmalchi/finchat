8c23751a78d5e4d51a2237fd2df9c7c1da43f1a8
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-07-16 15:13:54 +0900

    feat: `updated_at`, `created_at` 필드 추가

--- backend/app/schemas/chat.py
+++ backend/app/schemas/chat.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from typing import List, Literal, Optional, Union
 from pydantic import BaseModel, Field
+
+from pytz import timezone
 
 from app.schemas.product import PartialProductInfoDTO, ProductInfoDTO
 
@@ -46,6 +49,15 @@ class ChatPreviewDTO(BaseModel):
 
     chat_id: str
     title: str
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        json_encoders = {
+            datetime:
+                lambda v: v.astimezone(timezone('Asia/Seoul')).isoformat(
+                    timespec='milliseconds').replace('+00:00', 'Z')
+        }
 
 
 class ChatListResponse(BaseModel):
