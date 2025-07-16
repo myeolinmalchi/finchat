2c97d61e7214cfd4c05b2d77ab86d6f022748a92
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-07-16 13:01:50 +0900

    refactor: `tool_selection_node` -> `tool_node`
    도구 선택 및 실행 노드 통합

--- backend/domains/saving/agents/saving_subgraph.py
+++ backend/domains/saving/agents/saving_subgraph.py
@@ -7,7 +7,7 @@ from domains.common.agents.graph_state import GraphState
 
 from domains.saving.agents.router_node import init_router_node
 from domains.saving.agents.filter_node import init_filter_node
-from domains.saving.agents.tool_selection_node import init_saving_tool_seleciton_node
+from domains.saving.agents.tool_node import init_saving_tool_node
 
 
 def init_saving_subgraph(
@@ -18,22 +18,21 @@ def init_saving_subgraph(
 
     sg = StateGraph(GraphState)
 
-    sg.add_node("tool_selection_node",
-                init_saving_tool_seleciton_node(llm, saving_tools))
+    sg.add_node("tool_node", init_saving_tool_node(llm, saving_tools))
     #sg.add_node("tool_execution_node", init_saving_tool_execution_node(saving_tools))
     #sg.add_edge("tool_selection_node", "tool_execution_node")
 
     sg.add_node("filter_node", init_filter_node(llm))
     sg.add_node("router_node", init_router_node())
 
-    sg.add_edge("tool_selection_node", "filter_node")
+    sg.add_edge("tool_node", "filter_node")
 
     #sg.add_edge("tool_execution_node", "filter_node")
     sg.add_edge("filter_node", "router_node")
 
     sg.add_conditional_edges("router_node", lambda state: state["next"])
 
-    sg.set_entry_point("tool_selection_node")
+    sg.set_entry_point("tool_node")
     sg.set_finish_point("router_node")
 
     graph = sg.compile()
---
+++ backend/domains/saving/agents/tool_node.py
@@ -0,0 +1,56 @@
+from typing import Dict, List
+from langchain_core.language_models import BaseChatModel
+from langchain_core.tools import BaseTool
+from langgraph.config import get_stream_writer
+
+from domains.common.agents.graph_state import GraphState
+
+system_prompt = """\
+당신은 사용자의 요청과 researcher의 리서치 결과를 바탕으로
+상황에 맞는 적금 상품 검색 tool을 호출하는 에이전트다.
+
+사용자가 어떤 질문을 하더라도, 상품 검색을 수행해야 한다.
+올바른 파라미터와 함께 반드시 검색 도구를 호출하여라."""
+
+
+def init_saving_tool_node(llm: BaseChatModel, tools: List[BaseTool]):
+    """Tool 및 파라미터 결정 노드"""
+
+    agent_with_tools = llm.bind_tools(tools)
+    tool_map: Dict[str, BaseTool] = {t.name: t for t in tools}
+
+    async def node(state: GraphState):
+        writer = get_stream_writer()
+
+        writer({
+            "chat_id": state["chat_id"],
+            "status": "pending",
+            "content": {
+                "message": "상품을 검색하고 있습니다."
+            }
+        })
+
+        messages = [
+            {
+                "role": "system",
+                "content": system_prompt
+            },
+        ] + state["messages"]
+
+        res = await agent_with_tools.ainvoke(messages)
+        tool_call = res.tool_calls[0]  # type: ignore
+        tool = tool_map.get(tool_call.get("name"))
+        tool_args = {**tool_call.get("args", {}), "offset": state["offset"]}
+
+        if not tool:
+            raise ValueError
+
+        search_result = await tool.ainvoke(tool_args)
+        #state["messages"].append(res)
+
+        return {
+            "candidates": search_result,
+            "next": "filter_node",
+        }
+
+    return node
