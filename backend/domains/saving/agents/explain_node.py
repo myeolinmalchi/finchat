1bf9f1992b4f19c7170235e835ac73c54cc9bde1
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-07-20 15:55:54 +0900

    feat: include research context

--- backend/domains/saving/agents/explain_node.py
+++ backend/domains/saving/agents/explain_node.py
@@ -2,6 +2,7 @@ from typing import List
 from langchain_core.language_models import BaseChatModel
 from langchain_core.messages import AIMessage
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai.chat_models.base import ChatOpenAI
 from langgraph.config import get_stream_writer
 from langgraph.graph import END
 
@@ -12,15 +13,17 @@ from domains.saving.agents.prompts import (SAVING_EXPLAIN_NODE_SYSTEM_PROMPT,
 
 def init_explain_node(llm: BaseChatModel):
 
-    prompt_template = ChatPromptTemplate.from_messages([
-        ("system", SAVING_EXPLAIN_NODE_SYSTEM_PROMPT),
-        ("user", SAVING_EXPLAIN_USER_PROMPT_TEMPLATE)
-    ])
+    llm = ChatOpenAI(model="gpt-4.1", temperature=0.3)
 
     async def node(state: GraphState):
         writer = get_stream_writer()
 
         print("============ Explain Node ============")
+
+        prompt_template = ChatPromptTemplate.from_messages([
+            ("system", SAVING_EXPLAIN_NODE_SYSTEM_PROMPT),
+            ("user", SAVING_EXPLAIN_USER_PROMPT_TEMPLATE)
+        ])
 
         products = state["selected"]
 
@@ -51,11 +54,17 @@ def init_explain_node(llm: BaseChatModel):
             }
         })
 
-        blob: str = "\n---\n".join(list(map(str, products)))
+        research_blob = "\n\n## 외부 참고 정보\n" + "\n".join(
+            f"- {d}" for d in state["documents"])
+
+        blob: str = "\n---\n".join(map(str, products))
+
+        print(blob)
 
         prompt = prompt_template.invoke({
             "user_info": "",
             "product_info": blob,
+            "context": research_blob,
             "user_question": str(state["messages"][0].content),
         })
 
